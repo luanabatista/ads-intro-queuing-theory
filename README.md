01ba3c2a13773ee86c6708d4ad6fc5c971997e8c
Alaska <luanabatista979@gmail.com>
2022-08-22 12:45:37 -0300

    Adição de funções de cálculo básicas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ads-intro-queuing-theory
+
+
+
+
---
+++ main.py
@@ -0,0 +1,36 @@
+import math 
+
+#- a quantidade média de usuários que chegam por hora 165
+#- a capacidade de atendimento de cada funcionário 15
+#- a quantidade de atendentes no local 5
+
+# Taxa média de chegada (λ) em horas
+mediaChegada = 165
+
+# Pontos de atendimento
+atendentes = 5
+
+# Taxa média de atendimento do sistema Mi (μ) em horas
+mediaAtendimento = 15*atendentes
+
+#- o tempo de espera de um cliente nesta fila
+#- o tempo estimado de permanência no sistema
+
+#- Tempo médio de espera apenas na Fila (Wq)
+tempoFila = mediaChegada/(mediaAtendimento*(mediaAtendimento-mediaChegada))
+print(tempoFila)
+
+#- Tempo médio que um cliente permanece no sistema, esperando na Fila + Tempo de Atendimento (W)
+tempoSistema = 1/(mediaAtendimento-mediaChegada)
+print(tempoSistema)
+
+#- informa um cenário ideal para que se possa convocar mais funcionarios
+#- e caso não seja possivel escalar mais clientes, informa quantos cada um dos 5 teria que atender por hora
+
+# quantidade de funcionarios ideal 
+funcIdeal = math.ceil(mediaChegada/mediaAtendimento)
+print(funcIdeal)
+
+# quantidade ajuste atendimento por hora
+atendIdeal = math.ceil(mediaChegada/atendentes)
+print(atendIdeal)
